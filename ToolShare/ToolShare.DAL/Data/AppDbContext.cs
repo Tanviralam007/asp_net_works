d716a38b000f314642bba27247fc0165a3a0aa07
tanvir <tanviralam451@gmail.com>
2026-01-23 02:59:53 +0600

    Add AppDbContext class with DbSet properties for application entities

---
+++ ToolShare/ToolShare.DAL/Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToolShare.DAL.Entities;
+
+namespace ToolShare.DAL.Data
+{
+    public class AppDbContext : DbContext 
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        // tables
+        public DbSet<User> Users { get; set; }
+        public DbSet<ToolCategory> ToolCategories { get; set; }
+        public DbSet<Tool> Tools { get; set; }
+        public DbSet<BorrowRequest> BorrowRequests { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<ToolTransaction> ToolTransactions { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<ToolImage> ToolImages { get; set; }
+    }
+}
